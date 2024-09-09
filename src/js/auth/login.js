ac8b8f0c27d6bbcfd7932857bfcf10888631a848
Kadir Hacitemeloglu <kadir.hacitemeloglu@outlook.com>
2024-09-09 20:48:32 +0300

    fixin files.

--- src/js/auth/login.js
+++ src/js/auth/login.js
@@ -112,3 +112,15 @@ function showLoginPage() {
 }
 
 
+function navigateLogin(){
+  // Navigate to login page
+
+  // Implement additional logic here for navigation to login page
+  // For example, you can set a timeout to automatically redirect to login page after a certain period.
+   setTimeout(function(){
+      window.location.href = "index.html";
+   }, 2000);
+}
+
+
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
-function navigateLogin(){
+function navigateHomepage(){
     // Navigate to login page
 
     // Implement additional logic here for navigation to login page
     // For example, you can set a timeout to automatically redirect to login page after a certain period.
      setTimeout(function(){
-        window.location.href = "index.html";
+        window.location.href = "homepage.html";
      }, 2000);
 }
+
+
 
